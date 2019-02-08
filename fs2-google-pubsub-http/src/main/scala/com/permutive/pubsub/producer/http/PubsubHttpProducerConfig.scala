b9d92ce2290e235e95ccdc8f28eb48f2e8934c94
Paulius Imbrasas <paulius@permutive.com>
2019-02-08 14:00:09 +0000

    Add ScalaDoc to config

--- fs2-google-pubsub-http/src/main/scala/com/permutive/pubsub/producer/http/PubsubHttpProducerConfig.scala
+++ fs2-google-pubsub-http/src/main/scala/com/permutive/pubsub/producer/http/PubsubHttpProducerConfig.scala
@@ -2,6 +2,18 @@ package com.permutive.pubsub.producer.http
 
 import scala.concurrent.duration._
 
+/**
+  * Configuration for the PubSub HTTP producer.
+  *
+  * @param host                              host of PubSub
+  * @param port                              port of PubSub
+  * @param isEmulator                        whether the target PubSub is an emulator or not
+  * @param oauthTokenRefreshInterval         how often to refresh the Google OAuth token
+  * @param onTokenRefreshError               what to do if the token refresh fails
+  * @param oauthTokenFailureRetryDelay       initial delay for retrying OAuth token retrieval
+  * @param oauthTokenFailureRetryNextDelay   next delay for retrying OAuth token retrieval
+  * @param oauthTokenFailureRetryMaxAttempts how many times to attempt
+  */
 case class PubsubHttpProducerConfig[F[_]](
   host: String = "pubsub.googleapis.com",
   port: Int = 443,
