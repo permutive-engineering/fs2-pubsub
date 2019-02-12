f3e76dd0fa3fab2dfc074adb13e784637545144d
Tim Spence <timothywspence@gmail.com>
2019-02-12 14:49:25 +0000

    Add some standard typeclass instances
    - Functor for message decoder
    - Show for public-facing model types

--- fs2-google-pubsub/src/main/scala/com/permutive/pubsub/consumer/Model.scala
+++ fs2-google-pubsub/src/main/scala/com/permutive/pubsub/consumer/Model.scala
@@ -1,8 +1,21 @@
 package com.permutive.pubsub.consumer
 
+import cats.Show
+import cats.syntax.show._
+
 object Model {
   case class ProjectId(value: String) extends AnyVal
+  object ProjectId {
+    implicit val show: Show[ProjectId] = Show.fromToString
+  }
+
   case class Subscription(value: String) extends AnyVal
+  object Subscription {
+    implicit val show: Show[Subscription] = Show.fromToString
+  }
 
   case class Record[F[_], A](value: A, ack: F[Unit], nack: F[Unit])
+  object Record {
+    implicit def show[F[_], A : Show]: Show[Record[F, A]] = (record: Record[F, A]) => s"Record(${record.value.show})"
+  }
 }
--- fs2-google-pubsub/src/main/scala/com/permutive/pubsub/consumer/decoder/MessageDecoder.scala
+++ fs2-google-pubsub/src/main/scala/com/permutive/pubsub/consumer/decoder/MessageDecoder.scala
@@ -1,9 +1,18 @@
 package com.permutive.pubsub.consumer.decoder
 
+import cats.Functor
+
 trait MessageDecoder[A] {
   def decode(message: Array[Byte]): Either[Throwable, A]
 }
 
 object MessageDecoder {
   def apply[A: MessageDecoder]: MessageDecoder[A] = implicitly
+
+  implicit val functor: Functor[MessageDecoder] = new Functor[MessageDecoder] {
+
+    override def map[A, B](fa: MessageDecoder[A])(f: A => B): MessageDecoder[B] =
+      (message: Array[Byte]) => fa.decode(message).map(f)
+
+  }
 }
