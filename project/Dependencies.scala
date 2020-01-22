bfb697039e5d45a35bc30712a7c2cfcc9a0666c7
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-01-22 11:39:14 +0100

    Update java-jwt to 3.9.0 (#40)
    Co-authored-by: Travis Brown <travisrobertbrown@gmail.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     val fs2           = "2.2.1"
     val http4s        = "0.21.0-RC1"
     val log4cats      = "1.0.1"
-    val jwt           = "3.8.3"
+    val jwt           = "3.9.0"
     val jsoniter      = "2.1.5"
     val gcp           = "1.102.0"
     val scalatest     = "3.1.0"
