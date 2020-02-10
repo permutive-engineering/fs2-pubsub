e8a1b728baefc521f4ac3bd9a1efe97134538c00
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-02-10 03:49:30 +0100

    Update http4s-client, http4s-dsl, ... to 0.21.0 (#65)
    Co-authored-by: Travis Brown <travisrobertbrown@gmail.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,7 +7,7 @@ object Dependencies {
     val catsCore      = "2.1.0"
     val effect        = "2.1.1"
     val fs2           = "2.2.2"
-    val http4s        = "0.21.0-RC1"
+    val http4s        = "0.21.0"
     val log4cats      = "1.0.1"
     val jwt           = "3.9.0"
     val jsoniter      = "2.1.6"
