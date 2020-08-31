33b13739354fe805b99035b0ec7a98f0f4a24570
Scala Steward <me@scala-steward.org>
2020-08-31 04:44:47 +0200

    Update scalacheck-1-14 to 3.1.4.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
     val jsoniter       = "2.4.4"
     val gcp            = "1.107.0"
     val scalatest      = "3.1.2"
-    val scalatestPlus  = "3.1.2.0"
+    val scalatestPlus  = "3.1.4.0"
     val testContainers = "0.34.1"
   }
 
