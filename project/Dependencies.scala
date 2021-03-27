ff07946e15e3c4218bc77f564c2228ae5dfeef56
Scala Steward <me@scala-steward.org>
2021-03-27 20:36:55 +0100

    Update jsoniter-scala-core, ... to 2.7.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
     val http4s         = "0.21.19"
     val log4cats       = "1.1.1"
     val jwt            = "3.13.0"
-    val jsoniter       = "2.6.4"
+    val jsoniter       = "2.7.0"
     val gcp            = "1.111.2"
     val scalatest      = "3.2.5"
     val scalatestPlus  = "3.2.2.0"
