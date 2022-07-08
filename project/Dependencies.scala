d9c45216e9b8db5b67e7dde07c755ca30b041e9f
Scala Steward <scala_steward@virtuslab.com>
2022-07-08 00:45:52 +0000

    Update alleycats-core, cats-core to 2.8.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   object Versions {
     val scala212       = "2.12.14"
     val scala213       = "2.13.6"
-    val catsCore       = "2.6.1"
+    val catsCore       = "2.8.0"
     val effect         = "3.2.9"
     val fs2            = "3.1.2"
     val http4s         = "0.23.3"
