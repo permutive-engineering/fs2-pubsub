ddc848d59083a8319e85d48904d5531b705668ee
Scala Steward <scala_steward@virtuslab.com>
2022-07-08 00:45:28 +0000

    Update scala-library to 2.12.16

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   object Versions {
-    val scala212       = "2.12.14"
+    val scala212       = "2.12.16"
     val scala213       = "2.13.6"
     val catsCore       = "2.6.1"
     val effect         = "3.2.9"
