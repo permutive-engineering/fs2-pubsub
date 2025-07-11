8b85efc64a174326ac6d03ca1c7ed6bf006eecb2
Alejandro Hernández <info@alejandrohdezma.com>
2025-07-11 10:12:30 +0200

    Update Dependencies.scala
    Signed-off-by: Alejandro Hernández <info@alejandrohdezma.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
       ExclusionRule(organization = "com.thesamet.scalapb", name = "lenses_2.13"),
       ExclusionRule(organization = "com.thesamet.scalapb", name = "scalapb-runtime_2.13")
     ),
-    "com.permutive" %% "gcp-auth"            % "1.2.0",
+    "com.permutive" %% "gcp-auth"            % "2.0.1",
     "org.http4s"    %% "http4s-ember-client" % "0.23.30"
   )
 
@@ -24,22 +24,22 @@ object Dependencies {
 
   lazy val `fs2-pubsub` = Seq(
     "co.fs2"        %% "fs2-core"                % "3.12.0",
-    "com.permutive" %% "common-types-gcp-http4s" % "1.1.0",
+    "com.permutive" %% "common-types-gcp-http4s" % "1.2.0",
     "io.circe"      %% "circe-parser"            % "0.14.14",
     "org.http4s"    %% "http4s-circe"            % "0.23.30",
     "org.http4s"    %% "http4s-client"           % "0.23.30",
     "org.http4s"    %% "http4s-dsl"              % "0.23.30"
   ) ++ Seq(
     "com.dimafeng"  %% "testcontainers-scala-munit" % "0.43.0",
-    "com.permutive" %% "gcp-auth"                   % "1.2.0",
+    "com.permutive" %% "gcp-auth"                   % "2.0.1",
     "org.http4s"    %% "http4s-ember-client"        % "0.23.30",
     "org.slf4j"      % "slf4j-nop"                  % "2.0.17",
     "org.typelevel" %% "munit-cats-effect"          % "2.1.0"
   ).map(_ % Test)
 
   lazy val `fs2-pubsub-pureconfig` = Seq(
-    "com.github.pureconfig" %% "pureconfig-http4s"           % "0.17.7",
-    "com.permutive"         %% "common-types-gcp-pureconfig" % "1.1.0"
+    "com.github.pureconfig" %% "pureconfig-http4s"           % "0.17.9",
+    "com.permutive"         %% "common-types-gcp-pureconfig" % "1.2.0"
   )
 
 }
